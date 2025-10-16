6b050048feb87749abf5a5e8da89b8afbfd66f35
Saul Morales <moralespachecopablo@gmail.com.com>
2025-10-16 15:06:25 -0500

    Updated tests/wp-config.php

--- tests/wp-config.php
+++ tests/wp-config.php
@@ -34,7 +34,7 @@ define( 'WP_DEBUG', true );
 define( 'DB_NAME'       , getenv( 'WP_DB_NAME' ) ?: 'wp_test' );
 define( 'DB_USER'       , getenv( 'WP_DB_USER' ) ?: 'wp_test' );
 define( 'DB_PASSWORD'   , getenv( 'WP_DB_PASS' ) ?: '#dgs45As' );
-define( 'DB_HOST'       , getenv( 'WP_DB_HOST' ) ?: 'localhost' );
+define( 'DB_HOST'       , getenv( 'WP_DB_HOST' ) ?: '127.0.0.1' );
 define( 'DB_CHARSET'    , 'utf8' );
 define( 'DB_COLLATE'    , '' );
 
